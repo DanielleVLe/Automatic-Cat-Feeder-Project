899f1563a1b9d845c415e17b404509a46611b3a8
Danielle <danielle.le04@gmail.com>
2018-11-20 09:24:30 -0500

    Motor Controlling Code

---
+++ Motor_Direction_Contol.ino
@@ -0,0 +1,27 @@
+int in_1 = 3;
+int in_2 = 4;
+
+void setup()
+{
+  pinMode(in_1, OUTPUT);
+  pinMode(in_2, OUTPUT);
+}
+
+void motor (int direction) {
+  if (direction == 1) {
+    digitalWrite(in_1, HIGH);
+    digitalWrite(in_2, LOW);
+  }
+  else {
+    digitalWrite(in_1, LOW);
+    digitalWrite(in_2, HIGH);
+  }
+}
+
+void loop() {
+  motor (1);
+  delay(3000);
+  motor (2);
+  delay(3000); 
+}
+
---
+++ Motor_Speed_Control.ino
@@ -0,0 +1,30 @@
+int in_1 = 3;
+int in_2 = 4;
+
+void setup() {
+  pinMode(in_1, OUTPUT);
+  pinMode(in_2, OUTPUT);
+}
+
+void motor (int direction) {
+  if (if direction ==1) {
+    digitalWrite(in_1, HIGH);
+    digitalWrite(in_2, LOW);
+  }
+  else {
+    digitalWrite(in_1, LOW);
+    digitalWrite(in_2, HIGH);
+  }
+}
+
+void motor_speed(int speed_value) {
+  analogWrite(3, speed_value);
+  digitalWrite(4, direction);
+  
+  
+}
+
+void loop() {
+  motor (1);
+  delay(3000);
+}
